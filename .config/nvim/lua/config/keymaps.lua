764186a0b92c89afd444b68024b4cd5e0cacfecc
Leo <leo.minorui@gmail.com>
2024-09-14 22:44:08 +0800

    Modify configuration

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- windows resize cmd
+vim.keymap.set("n", "<S-Up>", ":resize +2<CR>")
+vim.keymap.set("n", "<S-Down>", ":resize -2<CR>")
+vim.keymap.set("n", "<S-Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<S-Right>", ":vertical resize +2<CR>")
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,7 +10,7 @@ opt.relativenumber = false
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff_lsp"
 
-vim.env.PATH = "~/.nvm/versions/node/v18.17.1/bin:" .. vim.env.PATH
+vim.env.PATH = "~/.nvm/versions/node/v18.17.1/bin:/Users/leo/.g/go/bin:" .. vim.env.PATH
 vim.filetype.add({
   pattern = {
     [".*/templates/.*%.yaml"] = "helm",
