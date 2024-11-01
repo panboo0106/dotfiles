868e0befcea640d1d09280c3961011b52ccf442c
Leo <leo.minorui@gmail.com>
2024-11-01 16:11:16 +0800

    Update nvim conf

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,10 +1,17 @@
 return {
   {
     "Exafunction/codeium.nvim",
-    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
     config = function()
-      require("codeium").setup({})
-      vim.g.codeium_enabled = false
+      require("codeium").setup({
+        enable_cmp_source = true,
+        virtual_text = {
+          enabled = false,
+        },
+      })
     end,
   },
 }
--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,20 +1,11 @@
 return {
   {
     "nvim-cmp",
-    dependencies = {
-      -- codeium
-      {
-        "Exafunction/codeium.nvim",
-        cmd = "Codeium",
-        build = ":Codeium Auth",
-        opts = {},
-      },
-    },
     opts = function(_, opts)
       table.insert(opts.sources, 1, {
         name = "codeium",
         group_index = 1,
-        priority = 100,
+        priority = 200,
       })
     end,
   },
